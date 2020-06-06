11e7ab75b4b3f19362af34623932318212ea910b
Manjunath973999 <manju831chandu@gmail.com>
2020-06-06 20:59:05 +0530

    Assignment-3

---
+++ Java Assignment 3a/src/com/ITO/Assignment3a/DetailsInXl.java
@@ -0,0 +1,286 @@
+package com.ITO.Assignment3a;
+
+public class DetailsInXl 
+{
+	private int business_partner_id;
+	private String business_partner_name;
+	private String business_partner_code;
+	private String contact_name;
+	private String address1;
+	private String address2;
+	private String city;
+	private String province;
+	private String country;
+	private String postal_code;
+	private String parent_company;
+	private String toll_free_number;
+	private int phone;
+	private String phone_extension;
+	private String fax;
+	private String website_url;
+	private String payment_condition;
+	private String gl_number;
+	private int driver_min_age;
+	private String api_enabled;
+	private String status;
+	private String one_way_free_paid_by;
+	private String country_code;
+	
+	public DetailsInXl()
+	{
+		this.business_partner_id = (Integer)null;
+		this.business_partner_name = null;
+		this.business_partner_code = null;
+		this.contact_name = null;
+		this.address1 = null;
+		this.address2 = null;
+		this.city = null;
+		this.province = null;
+		this.country = null;
+		this.postal_code = null;
+		this.parent_company = null;
+		this.toll_free_number = null;
+		this.phone = (Integer) null;
+		this.phone_extension = null;
+		this.fax = null;
+		this.website_url = null;
+		this.payment_condition = null;
+		this.gl_number = null;
+		this.driver_min_age = (Integer) null;
+		this.api_enabled = null;
+		this.status = null;
+		this.one_way_free_paid_by = null;
+		this.country_code = null;
+	}
+
+	public int getBusiness_partner_id() 
+	{
+		return business_partner_id;
+	}
+
+	public void setBusiness_partner_id(int business_partner_id) 
+	{
+		this.business_partner_id = business_partner_id;
+	}
+
+	public String getBusiness_partner_name() 
+	{
+		return business_partner_name;
+	}
+
+	public void setBusiness_partner_name(String business_partner_name) 
+	{
+		this.business_partner_name = business_partner_name;
+	}
+
+	public String getBusiness_partner_code() 
+	{
+		return business_partner_code;
+	}
+
+	public void setBusiness_partner_code(String business_partner_code) 
+	{
+		this.business_partner_code = business_partner_code;
+	}
+
+	public String getContact_name() 
+	{
+		return contact_name;
+	}
+
+	public void setContact_name(String contact_name) 
+	{
+		this.contact_name = contact_name;
+	}
+
+	public String getAddress1() 
+	{
+		return address1;
+	}
+
+	public void setAddress1(String address1) 
+	{
+		this.address1 = address1;
+	}
+
+	public String getAddress2() 
+	{
+		return address2;
+	}
+
+	public void setAddress2(String address2) 
+	{
+		this.address2 = address2;
+	}
+
+	public String getCity() 
+	{
+		return city;
+	}
+
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+
+	public String getProvince() 
+	{
+		return province;
+	}
+
+	public void setProvince(String province) 
+	{
+		this.province = province;
+	}
+
+	public String getCountry() 
+	{
+		return country;
+	}
+
+	public void setCountry(String country) 
+	{
+		this.country = country;
+	}
+
+	public String getPostal_code() 
+	{
+		return postal_code;
+	}
+
+	public void setPostal_code(String postal_code) 
+	{
+		this.postal_code = postal_code;
+	}
+
+	public String getParent_company() 
+	{
+		return parent_company;
+	}
+
+	public void setParent_company(String parent_company) 
+	{
+		this.parent_company = parent_company;
+	}
+
+	public String getToll_free_number() 
+	{
+		return toll_free_number;
+	}
+
+	public void setToll_free_number(String toll_free_number) 
+	{
+		this.toll_free_number = toll_free_number;
+	}
+
+	public int getPhone() 
+	{
+		return phone;
+	}
+
+	public void setPhone(int phone) 
+	{
+		this.phone = phone;
+	}
+
+	public String getPhone_extension() 
+	{
+		return phone_extension;
+	}
+
+	public void setPhone_extension(String phone_extension) 
+	{
+		this.phone_extension = phone_extension;
+	}
+
+	public String getFax() 
+	{
+		return fax;
+	}
+
+	public void setFax(String fax) 
+	{
+		this.fax = fax;
+	}
+
+	public String getWebsite_url() 
+	{
+		return website_url;
+	}
+
+	public void setWebsite_url(String website_url) 
+	{
+		this.website_url = website_url;
+	}
+
+	public String getPayment_condition() 
+	{
+		return payment_condition;
+	}
+
+	public void setPayment_condition(String payment_condition) 
+	{
+		this.payment_condition = payment_condition;
+	}
+
+	public String getGl_number() 
+	{
+		return gl_number;
+	}
+
+	public void setGl_number(String gl_number) 
+	{
+		this.gl_number = gl_number;
+	}
+
+	public int getDriver_min_age() 
+	{
+		return driver_min_age;
+	}
+
+	public void setDriver_min_age(int driver_min_age) 
+	{
+		this.driver_min_age = driver_min_age;
+	}
+
+	public String getApi_enabled() 
+	{
+		return api_enabled;
+	}
+
+	public void setApi_enabled(String api_enabled)
+	{
+		this.api_enabled = api_enabled;
+	}
+
+	public String getStatus() 
+	{
+		return status;
+	}
+
+	public void setStatus(String status) 
+	{
+		this.status = status;
+	}
+
+	public String getOne_way_free_paid_by() 
+	{
+		return one_way_free_paid_by;
+	}
+
+	public void setOne_way_free_paid_by(String one_way_free_paid_by) 
+	{
+		this.one_way_free_paid_by = one_way_free_paid_by;
+	}
+
+	public String getCountry_code() 
+	{
+		return country_code;
+	}
+
+	public void setCountry_code(String country_code) 
+	{
+		this.country_code = country_code;
+	}
+
+}
